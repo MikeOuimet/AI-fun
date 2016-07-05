aadf9e7f60f4e240c84bd6ec9bc2c64a9f89748d
Mike Ouimet <mike.ouimet@gmail.com>
2016-07-04 23:45:11 -0700

    fixed SARSA(lambda)

--- RLbasics/dieNTDL.py
+++ RLbasics/dieNTDL.py
@@ -8,17 +8,18 @@ to die roll unless roll a bad number, which makes you lose everything collected.
 
 #  Parameters
 N = 10
-e1 = .1
-alpha = .1
+e1 = .5
+alpha = .5
 gamma = 0.9
-lam = 1
+lam = .2
 bad = [10]
 go_on = True
-nruns = 500
+nruns = 5000
 
 
-endstate = 1000
+endstate = 500
 Q = np.zeros((endstate, 2))
+Ns = np.zeros((endstate, 2))
 
 
 #  TD(lambda)
@@ -33,7 +34,6 @@ def epsilongreedy(Qf, statef, epsilonf):
     else:
         a = np.random.randint(0, 2)
     return a
-
 
 # 1 = stop, 0 = bet
 
@@ -56,28 +56,29 @@ def environment(statef, a, bad):
 
 
 # + gamma*potential[newstate] -potential[state]
-def tdlambda(Q, s, a, r, ns, e, es):
+def tdlambda(Q, s, a, r, ns, e, es,Ns):
     Qnew = Q.copy()
-    diff = alpha*(r + gamma*(Q[ns, :].max()) - Q[s, a])
+    Ns[s, a] = Ns[s,a] + 1
+    aprime = epsilongreedy(Q, ns, e1)
+    diff = alpha/(Ns[s, a])*(r + gamma*(Q[ns, aprime]) - Q[s, a])
     for ss in range(es):
         for ac in range(2):
             if e[ss, ac] > 0:
                 Qnew[ss, ac] = Q[ss, ac] + diff*e[ss,ac]
-    return Qnew
+    return Qnew, aprime
 
 
 
-
+action = epsilongreedy(Q, state, e1)
 for run in range(nruns):
     eps = e1
     elig = np.zeros((endstate, 2))
     while go_on:
-        action = epsilongreedy(Q, state, eps)
         elig[state, action] = elig[state, action] + 1
         reward, go_on, newstate = environment(state, action, bad)
-        Qnew = tdlambda(Q, state, action, reward, newstate, elig, endstate)
-        #if run % 10000 == 0:
-        #    print run
+        Qnew, newaction = tdlambda(Q, state, action, reward, newstate, elig, endstate, Ns)
+        if run % 100 == 0:
+             print run
         #    print 'The state is %r and action is %r and prior Q is (%r %r)' % (state, action, Q[state, 0], Q[state, 1])
         #    print 'The reward is %r and the new state is %r' % (reward, newstate)
         #    print 'The Q of the new state %r is (%r %r)' % (newstate, Q[newstate, 0], Q[newstate,1])
@@ -86,13 +87,18 @@ for run in range(nruns):
         elig = gamma*lam*elig
         Q = Qnew
         state = newstate
+        action = newaction
     go_on = True
     state = 0 # np.random.randint(0, 6)
 
 printn = 100
 states = np.linspace(0, printn, printn+1)
+plt.subplot(2,1,1)
 plt.plot(states, Q[0:printn+1, 1])
 plt.plot(states, Q[0:printn+1, 0])
+plt.subplot(2,1,2)
+plt.plot(states, Ns[0:printn+1, 0])
+plt.plot(states, Ns[0:printn+1, 1])
 plt.show()
 
 
