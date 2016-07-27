676025aa8114662adeb9b40db5f498b8c611db3f
Michael T. Ouimet <ouimet@spawar.navy.mil>
2016-07-27 15:00:11 -0700

    added viz

--- RLbasics/RBF.py
+++ RLbasics/RBF.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 env = gym.make('MountainCar-v0')
 
@@ -8,7 +9,7 @@ def compute_fs(rbfss, obs, sig):
     err = rbfss - obs
     err = np.square(err)
     err = np.sum(err, axis=1)
-    return np.exp(-err / (2 * sig))
+    return np.exp(-err / (2 * sig**2))
 
 
 def epsilongreedy(Qf, fvals, epsilonf, num_actions):
@@ -24,98 +25,127 @@ def Qlookup(Qf, fvals, a):
     actionvals = np.dot(Qf, fvals)
     return actionvals[a]
 
+def visualize_v(rbfs, Q, flag):
+    activations = np.zeros((len(rbfs),len(rbfs)))
+    Vplot = np.zeros(len(rbfs))
+    for point in range(len(rbfs)):
+        activations[point, :] =  compute_fs(rbfs, rbfs[point], sig)
+        interim = []
+        for a in range(number_actions):
+            interim.append(Qlookup(Q, activations[point, :], a))
+        
+        Vplot[point] = np.max(interim)
+        
+    if flag:
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.scatter(rbfs[:,0], rbfs[:,1], Vplot)
+        plt.xlabel('position')
+        plt.ylabel('velocity')
+        plt.show()
+    #else:
+    #    fig = plt.figure()
+    #    plt.pcolor(rbfs[:,0], rbfs[:,1], Vplot)
+    #    plt.xlabel('position')
+    #    plt.ylabel('velocity')
+    #    plt.show()
+        #X,Y = np.mesh
+
+
 dim = 2
-number_rbfs = 1000
+number_rbfs = 400
 number_actions = 3
-eps = .5
-alpha = .005
-gamma = 0.95
-lam = .2
+eps = .1
+final_eps = .001
+NUM_EPISODES = 5000
+NUM_STEPS = 200
+decayrate = (final_eps/eps)**(1/(NUM_EPISODES+0.0))
 
-sig = .0025
-rbfs = np.random.uniform(-1, 1, (number_rbfs, dim))
+alpha = .01
+gamma = 0.99
+lam = .5
+scatter_flag = 1
+
+
+sig = 1.0/(np.sqrt(number_rbfs)-1.0)
+#rbfs = np.random.uniform(-1, 1, (number_rbfs, dim))
+rbfs = np.ones((number_rbfs, dim))
+xpoints= np.linspace(-1,1, np.sqrt(number_rbfs))
+
+count = 0
+for x in xpoints:
+    for y in xpoints:
+        rbfs[count,:] = [x,y]
+        count +=1
+
+print decayrate
 f_vals_done = np.zeros((number_rbfs))
-#rbfs[:, 0] = np.pi/10*rbfs[:, 0]
-#rbfs[:, 3] = 2.5*rbfs[:, 3]
 
-print env.observation_space.low
-print env.observation_space.high
-print env.action_space
+#print env.observation_space.low
+#print env.observation_space.high
+#print env.action_space
 
-Q = np.ones((number_actions, number_rbfs))
+Q = -10*np.ones((number_actions, number_rbfs))
 new_action = 0
 donedone = False
 count = 0
-NUM_EPISODES = 5000
-NUM_STEPS = 1000
+
+
+
 countvec = np.zeros(NUM_EPISODES)
 for rounds in range(NUM_EPISODES):
-    eps = eps*.999
+    eps = eps*decayrate
     elig = np.zeros((number_actions, number_rbfs))
     observation = env.reset()
-    #observation[0] = observation[0]/(.2) # normalize 15 degrees
-    observation[0] = (observation[0] + .3)
+    observation[0] = (observation[0] + .3)/(.9)
     observation[1] = 12*observation[1]
-    #observation[3] = observation[3]/(2.4)
-    
-    #print 'observation', observation
     f_vals = compute_fs(rbfs, observation, sig)
-    #if donedone and rounds % 10 ==0:
-    #    print 'Most recent goal Q', np.dot(Q, f_vals_done)
-    #    print 'Starting state Q', np.dot(Q, f_vals)
-    #    print ''
-    #print 'activations', f_vals
     action = epsilongreedy(Q, f_vals, eps, number_actions)
     for t in range(NUM_STEPS):
-        if rounds % 100 ==0:
-            env.render()
+        #if rounds % 1000 ==0:
+        #    env.render()
         new_observation, reward, done, info = env.step(action)
-        new_observation[0] = (new_observation[0] + .3)
+        new_observation[0] = (new_observation[0] + .3)/(.9)
         new_observation[1] = 12*new_observation[1]
-        #if done:        
-            #print 'observation', new_observation
-            #print 'possible action values', np.dot(Q,f_vals)
-            #print 'action', action
-        
-        elig[action, :] = elig[action, :] + f_vals
+        elig[action, :] = f_vals
         #print 'maximum eligibilities', elig.max()
-        f_vals_new = compute_fs(rbfs, new_observation, sig)
+        f_vals_new = compute_fs(rbfs, new_observation, sig)     
         
         new_action = epsilongreedy(Q, f_vals_new, eps, number_actions)
         Qnewaction = Qlookup(Q, f_vals_new, new_action)
-        #if done:
-            #print 'Q of new action', Qnewaction
 
         TDerror = reward + gamma*Qnewaction - Qlookup(Q, f_vals, action)
-        #if done:
-        #    TDerror = reward - Qlookup(Q, f_vals, action)
+        if done:
+            TDerror = reward  - Qlookup(Q, f_vals, action)
         
-        #if done:
-        #    print eps
-        #    print 'Q before', Qlookup(Q, f_vals, action)
-            #print t
-            #print 'TD error: ', TDerror
-            #print 'maximum elig. ', elig.max()
-            #print 'action:, ', action
+        Qbefore = Qlookup(Q, f_vals, action)
 
         Q[action, :] = Q[action, :] + alpha*TDerror*elig[action, :]
         #if done:
         #    print 'Q after', Qlookup(Q, f_vals, action)
         
         TDerrornew = reward + gamma*Qnewaction - Qlookup(Q, f_vals, action)
-        #if done:
-        #    TDerrornew = reward - Qlookup(Q, f_vals, action)
+        if done:
+            TDerrornew = reward - Qlookup(Q, f_vals, action)
+        Qafter = Qlookup(Q, f_vals, action)
         
-        elig[:] = lam*gamma*elig
-        f_vals[:] = f_vals_new
-        #if done:
-            #print 'reward:', reward
+        #TDerror - TDerrornew 
+        
+        if done and rounds % 100 == 0:
+            print 'time:', t
+            print 'observation', new_observation
+            print 'reward:', reward
             #print 'Target: ', reward + gamma*Qnewaction
             #print 'max TD update', np.abs(alpha*TDerror*elig[action, :]).max()
             #print 'min TD update', np.abs(alpha*TDerror*elig[action, :]).min()
-            #print 'TD error before', TDerror
-            #print 'TD error after', TDerrornew
-            
+            print 'max eligibility', elig[action, :].max()
+            print 'TD error before', TDerror
+            print 'TD error after', TDerrornew
+            print 'Q before', Qbefore
+            print 'Q after', Qafter
+            print ''
+        elig[:] = lam*gamma*elig
+        f_vals[:] = f_vals_new
         action = new_action
         if done:
             count +=1
@@ -125,7 +155,7 @@ for rounds in range(NUM_EPISODES):
             #f_vals_done[:] = f_vals
             #print ''
             break
-        if Q.max() > 1000:
+        if Qafter > 100:
             print 'Q diverged'
             break
     if done:
@@ -135,8 +165,11 @@ for rounds in range(NUM_EPISODES):
     if rounds % 100 ==0:
         print rounds
         if rounds >0:
+            print 'end Q', 
             print 'Fraction completed:', count/(rounds +0.0)
+            visualize_v(rbfs, Q, scatter_flag)
             print ''
+            
 
 print 'Fraction completed:', count/(rounds +0.0)
 
