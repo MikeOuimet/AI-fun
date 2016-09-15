093a24f4c7ddfaf0e47b3709f4e38f0e4a1fde1a
Mike Ouimet <mike.ouimet@gmail.com>
2016-09-14 21:40:51 -0700

    updated kmeans

--- compression/kmeans.py
+++ compression/kmeans.py
@@ -10,33 +10,87 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from sklearn import metrics
+from scipy import ndimage
 
 
+def meandist2points(image, kpoints):
+    length = np.max(np.shape(image))
+    dists =metrics.pairwise.euclidean_distances(image, kpoints)**2
+    nearest = np.argmin(dists, axis =1)
+    vecdists = []
+    for pixel in range(length):
+        val = image[pixel, :] - kpoints[nearest[pixel],:] + 0.0
+        vecdists.append(np.sqrt(val.dot(val)))
+        #sum_of_dists += np.sqrt(val.dot(val))
+    #return sum_of_dists/length
+    return np.mean(vecdists)
 
-img = np.load('frame.npy')
-new_img = np.reshape(img, (480*640, 3))
 
-k = 16
+img = ndimage.imread('fw2.png')
+
+img_height = np.shape(img)[0]
+img_width = np.shape(img)[1]
+
+plt.imshow(img)
+
+new_img = np.reshape(img, (img_width*img_height, 3))
+
+k = 64
 
 kvec = np.random.uniform(0, 255, (k, 3))
-
-for step in range(10):
-    print step
+err = meandist2points(new_img, kvec)
+while True:
     dists =metrics.pairwise.euclidean_distances(new_img, kvec)**2
 
     nearest = np.argmin(dists, axis =1)
+    #bestdists = np.min(dists,axis =1)
+    #avg_dists = np.mean(bestdists)
+    #print avg_dists
+    #print  meandist2points(new_img, kvec)
 
-    print kvec
+    #print kvec
     for means in range(k):
-        new_point = np.nan_to_num(np.mean(new_img[nearest== means, :],axis= 0))
-        if new_point[0] == 0:
-            new_point = np.random.uniform(0, 255, (1, 3))
+        new_point = np.mean(new_img[nearest== means, :],axis= 0)
+        if new_point[0] != new_point[0]: # nan because no pixels are with this point
+            new_point = np.random.uniform(0, 255, (1, 3))          
+        #new_point = np.nan_to_num(np.mean(new_img[nearest== means, :],axis= 0))
+        #if new_point[0] < 1 and new_point[1] < 1 and new_point[2] < 1:
+        #    new_point = np.random.uniform(0, 255, (1, 3))
         kvec[means, :] = new_point
 
+    err2 = meandist2points(new_img, kvec)
+    print err2
+    print ''
+    if np.abs(err2 - err) < .1:
+        break
+    err = err2
 
-#npoints = 50000
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-ax.scatter(kvec[:,0], kvec[:, 1], kvec[:, 2]  )
-plt.show()
+
+
+kfinal = np.round(kvec)
+dists =metrics.pairwise.euclidean_distances(new_img, kfinal)
+
+nearest = np.argmin(dists, axis =1)
+
+#print kvec
+compressed_img = np.zeros((img_width*img_height, 3))
+#compressed_img[:] = new_img
+for means in range(k):
+    compressed_img[nearest == means, :] = kfinal[means]
+
+all_black = 255*np.ones((img_height,img_width,3))
+final_img = np.reshape(compressed_img, (img_height,img_width,3))
+plt.imshow(-final_img)
+
+residual = np.abs(compressed_img - new_img)
+residual = np.sum(residual, axis = 1)
+plt.hist(residual, 20)
+
+#plt.imshow(final_img)
+
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111, projection='3d')
+#ax.scatter(kvec[:,0], kvec[:, 1], kvec[:, 2]  )
+#plt.show()
 
