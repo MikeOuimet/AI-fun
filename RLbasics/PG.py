2cd2bb69c2baa76d0ca3e18b9ad75ec4d1761ca9
Mike Ouimet <mike.ouimet@gmail.com>
2016-09-19 21:07:49 -0700

    gradient seems to be working

--- RLbasics/PG.py
+++ RLbasics/PG.py
@@ -17,17 +17,19 @@ env = gym.make('CartPole-v0')
 dim = max(np.shape(env.observation_space))
 dim_actions = env.action_space.n
 
-num_nodes = 5
-n_timesteps = 3
-num_gradients =50
+num_nodes = 50
+
+num_gradients =200
 maxsteps = 400
 num_runs = 1
 
-sess = tf.InteractiveSession()
+#sess = tf.InteractiveSession()
 
 state = tf.placeholder(tf.float32, shape=[None, dim])
 action_choice = tf.placeholder(tf.float32, shape=[None, dim_actions])
-reward_signal = tf.placeholder(tf.float32, shape=[None,1])
+reward_signal = tf.placeholder(tf.float32, shape=(None,1) )
+#n_timesteps = tf.placeholder(tf.int16, shape = ())
+
 
 W1 = weight_variable([dim, num_nodes])
 b1 = bias_variable([num_nodes])
@@ -37,47 +39,55 @@ Wo = weight_variable([num_nodes, dim_actions])
 bo = bias_variable([dim_actions])
 ao = tf.nn.softmax(tf.matmul(a1, Wo) + bo)
 
+#blah = tf.matmul(ao,tf.transpose(action_choice))
+
+log_prob1 = tf.matmul(ao,tf.transpose(action_choice))
+log_prob2 = tf.diag_part(tf.matmul(ao,tf.transpose(action_choice)))
 log_prob = tf.log(tf.diag_part(tf.matmul(ao,tf.transpose(action_choice))))# fix this so it doesn't need diag
-log_prob = tf.reshape(log_prob, (1,n_timesteps)) # how can I matrix multiply without hardcode reshaping
-loss =  -tf.matmul(log_prob, reward_signal)/n_timesteps
-train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+log_prob = tf.reshape(log_prob, (1,-1)) # how can I matrix multiply without hardcode reshaping
+loss =  -tf.matmul(log_prob, reward_signal)
+loss = tf.reshape(loss, [-1])
+train_step = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
 init = tf.initialize_all_variables()
 sess = tf.Session()
 sess.run(init)
 
+
+
 for run in range(num_runs):
 
-    states = np.zeros((maxsteps,dim))
-    actions = np.zeros((maxsteps,dim_actions))
-    rewards = np.zeros((maxsteps,1))
-    time =0
+    states = np.zeros((maxsteps,dim), dtype='float32')
+    actions = np.zeros((maxsteps,dim_actions), dtype='float32')
+    rewards = np.zeros((maxsteps,1), dtype='float32')
+    timestep =0
     observation = env.reset()
     observation = np.reshape(observation,(1,dim))
     done = False
     
-    while not done and time < maxsteps:
+    while not done and timestep < maxsteps:
         action_prob = sess.run(ao, feed_dict={state: observation})
-        action_choice = np.argmax(np.random.multinomial(1, action_prob[0]))
-        new_observation, reward, done, info = env.step(action_choice)
+        action = np.argmax(np.random.multinomial(1, action_prob[0]))
+        new_observation, reward, done, info = env.step(action)
         
-        states[time, :]= observation
+        states[timestep, :]= observation
     
-        actions[time, action_choice] = 1
-        rewards[time, :] = reward
-        time +=1
+        actions[timestep, action] = 1
+        rewards[timestep,:] = reward
+        timestep +=1
         
         observation[:] = new_observation
 
-states = states[:time, :]
-actions = actions[:time, :]
-rewards = rewards[:time,:]
+states = states[:timestep, :]
+actions = actions[:timestep, :]
+rewards = rewards[:timestep,:]
 
 
 print sess.run(loss, feed_dict={state: states, action_choice: actions, reward_signal: rewards})
+
 for i in range(num_gradients):
     sess.run(train_step, feed_dict={state: states, action_choice: actions, reward_signal: rewards})
     print sess.run(loss, feed_dict={state: states, action_choice: actions, reward_signal: rewards})
-print sess.run(ao, feed_dict={state: states})
+#print sess.run(ao, feed_dict={state: states})
 
 env.render(close=True)
 
