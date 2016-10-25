6b56d1d237cee8ef21f020022870cdbbd6c841ba
Mike <mike.ouimet@gmail.com>
2016-10-24 22:42:18 -0700

    maybe working?

--- RLbasics/PG_doom.py
+++ RLbasics/PG_doom.py
@@ -53,9 +53,9 @@ discount_factor = .999
 
 patch_size = 2
 
-num_gradients = 10
-maxsteps = 100
-num_runs = 1
+num_gradients = 2
+maxsteps =400
+num_runs = 1000
 nskips = 4
 
 height_start = 180
@@ -102,8 +102,8 @@ ao = tf.nn.softmax(tf.matmul(h_fc1, W_fc2) + b_fc2)
 log_prob = tf.log(tf.diag_part(tf.matmul(ao, tf.transpose(action_choice))))# fix this so it doesn't need diag
 log_prob = tf.reshape(log_prob, (1,-1))
 loss = tf.matmul(log_prob, advantage_signal)
-loss = -tf.reshape(loss, [-1])
-train_step = tf.train.AdamOptimizer().minimize(loss)
+loss = tf.reshape(loss, [-1]) # is abs right?
+train_step = tf.train.RMSPropOptimizer(1e-4).minimize(loss)
 
 # Value End of network
 W_fc2_value = weight_variable([1024, 1])
@@ -132,7 +132,7 @@ for rs in range(1,maxsteps):
 for run in range(num_runs):
 	#original_state = np.zeros((1, height_end-height_start, width_end-width_start, 3), dtype='uint8')
 	cropped_observation = np.zeros((1,height_end-height_start,width_end-width_start,3))
-	states = np.zeros((maxsteps, 35, 270, 3), dtype='uint8')
+	states = np.zeros((maxsteps, 35, 270, 3), dtype='float32')
 	actions = np.zeros((maxsteps,3), dtype='uint8')
 	rewards = np.zeros((maxsteps,1), dtype='float32')
 	weighted_sum_rewards = np.zeros((maxsteps,1), dtype='float32')
@@ -165,18 +165,18 @@ for run in range(num_runs):
 				action_vec[10] = 1 #right
 			elif action == 2:
 				action_vec[11] = 1 #left
-			print 'NEW ACTION'
-		print '{} = skip count'.format(skip_count)
+		#	print 'NEW ACTION'
+		#print '{} = skip count'.format(skip_count)
 		skip_count += 1
 		if skip_count >= nskips:
 			skip_count = 0
-		print 'The action is {}'.format(action)
-		print 'The timestep is {}'.format(timestep)
-		print ''
+		#print 'The action is {}'.format(action)
+		#print 'The timestep is {}'.format(timestep)
+		#print ''
 
 		observation, reward, done, info = env.step(action_vec) # need to add all the data of the skips into history
 		observation = np.reshape(observation, (1, 480, 640, 3))
-		cropped_observation[:] = observation[:,height_start:height_end,width_start:width_end,:]
+		cropped_observation[:] = observation[:,height_start:height_end,width_start:width_end,:]/255.0
 		pooled_picture = sess.run(h_prepool2, feed_dict={original_state: cropped_observation})
 		
 
@@ -187,8 +187,8 @@ for run in range(num_runs):
 	print 'The run lasted {} timesteps'.format(timestep_of_run)
 	states = states[:timestep_of_run, :]
 	actions = actions[:timestep_of_run, :]
-	rewards = rewards[:timestep_of_run,:]
-	print actions
+	rewards = rewards[:timestep_of_run,:]/200
+	#print actions
 	#summed_rewards[:, 0] = np.cumsum(rewards[::-1])[::-1]
 
 	for step in range(timestep_of_run):
@@ -200,7 +200,7 @@ for run in range(num_runs):
 	current_values = sess.run(ao_value, feed_dict={state: states})
 	#print current_values
 	#print ''
-	advantages = final_rewards - current_values
+	advantages = (final_rewards - current_values)
 	#print advantages
 
 	print 'value function loss'
@@ -221,10 +221,13 @@ for run in range(num_runs):
     #    print ''
 	print 'policy function loss'
 	print sess.run(loss, feed_dict={state: states, action_choice: actions, advantage_signal: advantages})
+	#print sess.run(ao, feed_dict={state: states, action_choice: actions, advantage_signal: advantages})
+	#print actions
+	#print sess.run(log_prob, feed_dict={state: states, action_choice: actions, advantage_signal: advantages})
 	for i in range(num_gradients):
 		sess.run(train_step, feed_dict={state: states, action_choice: actions, advantage_signal: advantages})
 		print sess.run(loss, feed_dict={state: states, action_choice: actions, advantage_signal: advantages})
-    #print ''
+	print ''
 
 '''
 #timestep_learning[run] = timestep
